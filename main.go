feda801737ab41d0ca7cb5e8afd5e2cd604b264a
Michael Santos <michael.santos@gmail.com>
2026-04-01 07:54:57 -0400

    0.3.0

--- main.go
+++ main.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2020-2024, Michael Santos <michael.santos@gmail.com>
+// Copyright (c) 2020-2026, Michael Santos <michael.santos@gmail.com>
 //
 // Permission to use, copy, modify, and/or distribute this software for any
 // purpose with or without fee is hereby granted, provided that the above
@@ -60,7 +60,7 @@ type argvT struct {
 }
 
 const (
-	version = "0.2.1"
+	version = "0.3.0"
 
 	Akamai     = "akamai"
 	Cloudflare = "cloudflare"
